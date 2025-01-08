e9a768ab776095ef54e405ff939d3fac2d1529a1
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-08 11:59:16 +0100

    Use this branch

--- review.bash
+++ review.bash
@@ -7,7 +7,7 @@ output_dir="$(mktemp --directory)"
 
 gh workflow run 'nixpkgs-review.yml' --field pr-number="$pr_number"
 sleep 10 # TODO: Ensure to get correct ID
-run_id="$(gh run list --workflow=nixpkgs-review.yml --branch main --limit 1 --json databaseId --jq '.[].databaseId')"
+run_id="$(gh run list --workflow=nixpkgs-review.yml --branch improve-security --limit 1 --json databaseId --jq '.[].databaseId')"
 
 gh run watch "$run_id" # Don't use --exit-status to make sure the downloading
 gh run download "$run_id" --dir "$output_dir"
