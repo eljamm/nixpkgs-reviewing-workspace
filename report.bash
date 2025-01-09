ab1a88c364d16651ce9aa39b216545ff6c7f67de
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-09 12:58:30 +0100

    Make report script comment more clear

--- report.bash
+++ report.bash
@@ -13,7 +13,8 @@ echo >"$report" # clear old report, if it exists
 for runner in X64-Linux X64-macOS ARM64-macOS; do
     runner_report=$(cat "$output_dir/nixpkgs-review-files-pr-$pr_number-$runner/pr-$pr_number/report.md")
 
-    # Remove redundant summary from subsequent runner results
+    # The `nixpkgs-review` header is included with the first runner (X64-Linux)
+    # so we don't need to include the ones from subsequent runners.
     if [ "$runner" != "X64-Linux" ]; then
         runner_report=$(echo "$runner_report" | sed '1,5d')
     fi
