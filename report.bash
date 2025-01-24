d4f3d82ee6b56f89cd8f3ca3bb84d138df578128
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-25 00:38:03 +0100

    feat: only fetch report artifacts from review
    This makes collecting the final results faster since we don't have to
    wait until all the files are downloaded.

--- report.bash
+++ report.bash
@@ -3,15 +3,27 @@
 set -euxo pipefail
 
 pr_number="$1"
-output_dir="$2"
+run_id="$2"
+output_dir="$3"
+
+download_cmd=""
 report="$output_dir/report.md"
+
+runners="X64-Linux ARM64-Linux X64-macOS ARM64-macOS"
+
+for runner in $runners; do
+    download_cmd="$download_cmd -n nixpkgs-review-report-pr-${pr_number}-${runner}"
+done
+
+gh run download "$run_id" $download_cmd --dir "$output_dir"
+echo "Downloaded the files in $output_dir"
 
 tree "$output_dir"
 
 echo >"$report" # clear old report, if it exists
 
-for runner in X64-Linux ARM64-Linux X64-macOS ARM64-macOS; do
-    runner_report=$(cat "$output_dir/nixpkgs-review-files-pr-$pr_number-$runner/pr-$pr_number/report.md")
+for runner in $runners; do
+    runner_report=$(cat "$output_dir/nixpkgs-review-report-pr-${pr_number}-${runner}/report.md")
 
     # The `nixpkgs-review` header is included with the first runner (X64-Linux)
     # so we don't need to include the ones from subsequent runners.
--- watch.bash
+++ watch.bash
@@ -6,12 +6,11 @@ pr_number="$1"
 output_dir="$(mktemp --directory)"
 
 sleep 10 # NOTE: to ensure that the correct ID is fetched
-# TODO: get ID from PR number?
+
 run_id="$(gh run list --workflow=nixpkgs-review.yml --branch improve-security --limit 1 --json databaseId --jq '.[].databaseId')"
 
 gh run watch "$run_id" # Don't use --exit-status to make sure the downloading
-gh run download "$run_id" --dir "$output_dir"
 
-echo "Downloaded the files in $output_dir"
+exec ./report.bash "$pr_number" "$run_id" "$output_dir"
 
-exec ./report.bash "$pr_number" "$output_dir"
+# TODO: interactively download full artifacts
